a9415ff3de175bed44240ef6503b7d2480b4f6f1
Justin <justin_ellery@hotmail.ca>
2016-07-04 20:24:18 -0400

    HN Scraper of top links added, working on NHL scraping 4FUN.

--- server.js
+++ server.js
@@ -1,97 +1,37 @@
-var request = require("request"),
-	cheerio = require("cheerio"),
-	url = "https://www.google.com/search?q=data+mining",
-	corpus = {},
-	totalResults = 0,
-	resultsDownloaded = 0;
+var request = require('request');
+var cheerio = require('cheerio');
+var jsonfile = require('jsonfile')
+var file = './logs/data.json'
 
-function callback () {
-	resultsDownloaded++;
+// make a request to the hardcoded link
+request('http://www.hockey-reference.com/teams/PIT/2016.html', function(error, response, html) {
+    if (!error && response.statusCode == 200) {
 
-	if (resultsDownloaded !== totalResults) {
-		return;
-	}
+        // get html from cheerio
+        var $ = cheerio.load(html);
 
-	var words = [];
+        // array to hold each stat with name and tip
+        var stats = [];
 
-	// stick all words in an array
-	for (prop in corpus) {
-		words.push({
-			word: prop,
-			count: corpus[prop]
-		});
-	}
+        $('#team_stats thead tr th').each(function(i, element) {
+            // stat-name
+            var statName = $(this).data('stat');
+            // stat-tip
+            var statTip = $(this).data('tip');
 
-	// sort array based on how often they occur
-	words.sort(function (a, b) {
-		return b.count - a.count;
-	});
+            // Our parsed meta data object
+            var metadata = {
+                name: statName,
+                tip: statTip
+            };
 
-	// finally, log the first fifty most popular words
-	console.log(words.slice(0, 20));
-}
+            // Push it to our results
+            stats.push(metadata);
+        });
 
-request(url, function (error, response, body) {
-	if (error) {
-		console.log("Couldn’t get page because of error: " + error);
-		return;
-	}
-
-	// load the body of the page into Cheerio so we can traverse the DOM
-	var $ = cheerio.load(body),
-		links = $(".r a");
-
-	links.each(function (i, link) {
-		// get the href attribute of each link
-		var url = $(link).attr("href");
-
-		// strip out unnecessary junk
-		url = url.replace("/url?q=", "").split("&")[0];
-
-		if (url.charAt(0) === "/") {
-			return;
-		}
-
-		// this link counts as a result, so increment results
-		totalResults++;
-
-		// download that page
-		request(url, function (error, response, body) {
-			if (error) {
-				console.log("Couldn’t get page because of error: " + error);
-				return;
-			}
-
-			// load the page into cheerio
-			var $page = cheerio.load(body),
-				text = $page("body").text();
-
-			// throw away extra whitespace and non-alphanumeric characters
-			text = text.replace(/\s+/g, " ")
-					   .replace(/[^a-zA-Z ]/g, "")
-					   .toLowerCase();
-
-			// split on spaces for a list of all the words on that page and
-			// loop through that list
-			text.split(" ").forEach(function (word) {
-				// we don't want to include very short or long words, as they're
-				// probably bad data
-				if (word.length < 4 || word.length > 20) {
-					return;
-				}
-
-				if (corpus[word]) {
-					// if this word is already in our "corpus", our collection
-					// of terms, increase the count by one
-					corpus[word]++;
-				} else {
-					// otherwise, say that we've found one of that word so far
-					corpus[word] = 1;
-				}
-			});
-
-			// and when our request is completed, call the callback to wrap up!
-			callback();
-		});
-	});
+        // Write the json to a file
+        jsonfile.writeFile(file, parsedResults, function(err) {
+            console.error(err)
+        });
+    }
 });
